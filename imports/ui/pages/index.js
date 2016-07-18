c49bf2864208f9f7926653d14543237a527a7e41
Michal Takac <takacmichal92@gmail.com>
2016-07-18 04:29:50 +0200

    add repo link

--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -6,6 +6,6 @@ export const Index = () => (
     <h2>MeteorVR</h2>
     <p>Work-in-progress. Using A-Frame for web-based VR. Trying to achieve multiuser
     VR experience in browser. Not working as expected yet!</p>
-    <p>You can help by contributing to <a href="">this</a> repo!</p>
+    <p>You can help by forking <a href="https://github.com/DimensionLab/meteor-vr">this</a> repo and contributing!</p>
   </Jumbotron>
 );
